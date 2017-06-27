847eca5165fa2a5e096888824ec274bb7b3e9355
Visar Zejnullahu <visar.zejnullahu@gmail.com>
2017-06-27 18:38:47 +0200

    Implement MSISDN to MNO, country dialing number, country identifier subscriber number

--- parse_msisdn.py
+++ parse_msisdn.py
@@ -1,12 +1,40 @@
 class ParseMSISDN(object):
     @classmethod
     def transform(cls, msisdn):
+        if msisdn[0] == '+':
+            msisdn = msisdn[1:]
         if not msisdn.isdigit():
             raise ValueError("Input parameter must be integer")
         if len(msisdn) > 15:
             raise ValueError("Input should be 15 digits at most")
-        result = dict(mno_identifier=0,
-                      country_dialing_code=0,
-                      subscriber_number=0,
-                      country_identifier=0,)
-        return result
+
+        country_codes = dict()
+        mno_providers = dict()
+
+        with open('country_codes', 'r') as f:
+            for line in f:
+                (key, val) = line.split(' ', 1)
+                country_codes[key.strip()] = val.strip()
+
+        with open('mno_providers', 'r') as f:
+            for line in f:
+                (key, val) = line.split(' ', 1)
+                mno_providers[key.strip()] = val.strip()
+
+        for length in range(1, 5):
+            if msisdn[0: length] in country_codes:
+                country_identifier = country_codes[msisdn[0: length]]
+                country_dialing_code = msisdn[0: length]
+                for operator_length in range(1, 5):
+                    if msisdn[0: length + operator_length] in mno_providers:
+                        mno_identifier = mno_providers[
+                            msisdn[0: length + operator_length]]
+                        subscriber_number = msisdn[length + operator_length:]
+
+        if mno_identifier is None or subscriber_number is None:
+            raise Exception("Invalid number")
+
+        return dict(mno_identifier=mno_identifier,
+                    country_dialing_code=country_dialing_code,
+                    subscriber_number=subscriber_number,
+                    country_identifier=country_identifier,)
--- test/test_parse_msisdn.py
+++ test/test_parse_msisdn.py
@@ -4,12 +4,17 @@ from parse_msisdn import ParseMSISDN
 
 class TestParseMSISDN(unittest.TestCase):
     def test_result_is_dict(self):
-        msisdn = "123"
+        msisdn = "38976123456"
+        result = ParseMSISDN.transform(msisdn)
+        self.assertIsInstance(result, dict)
+
+    def test_leading_plus(self):
+        msisdn = "+38976123456"
         result = ParseMSISDN.transform(msisdn)
         self.assertIsInstance(result, dict)
 
     def test_check_keys(self):
-        msisdn = "123"
+        msisdn = "38976123456"
         result = ParseMSISDN.transform(msisdn)
         self.assertIn('mno_identifier', result.keys())
         self.assertIn('country_dialing_code', result.keys())
@@ -27,7 +32,11 @@ class TestParseMSISDN(unittest.TestCase):
     def test_country_dialing_code(self):
         msisdn = "38976123456"
         result = ParseMSISDN.transform(msisdn)
-        print(result)
+        self.assertEqual(result['country_dialing_code'], "389")
+
+    def test_country_dialing_code_with_leading_plus(self):
+        msisdn = "+38976123456"
+        result = ParseMSISDN.transform(msisdn)
         self.assertEqual(result['country_dialing_code'], "389")
 
     def test_country_identifier(self):
@@ -35,21 +44,46 @@ class TestParseMSISDN(unittest.TestCase):
         result = ParseMSISDN.transform(msisdn)
         self.assertEqual(result['country_identifier'], "MK")
 
+    def test_country_identifier_with_leading_plus(self):
+        msisdn = "+38976123456"
+        result = ParseMSISDN.transform(msisdn)
+        self.assertEqual(result['country_identifier'], "MK")
+
     def test_mno_identifier(self):
         msisdn = "38976123456"
         result = ParseMSISDN.transform(msisdn)
-        self.assertEqual(result['mno_identifier'], "VIP MK")
+        self.assertEqual(result['mno_identifier'], "VIP Operator")
+
+    def test_mno_identifier_with_leading_plus(self):
+        msisdn = "+38976123456"
+        result = ParseMSISDN.transform(msisdn)
+        self.assertEqual(result['mno_identifier'], "VIP Operator")
 
     def test_subscriber_number(self):
         msisdn = "38976123456"
         result = ParseMSISDN.transform(msisdn)
-        self.assertEqual(result['subscriber_number'], "200527")
+        self.assertEqual(result['subscriber_number'], "123456")
+
+    def test_subscriber_number_with_leading_plus(self):
+        msisdn = "+38976123456"
+        result = ParseMSISDN.transform(msisdn)
+        self.assertEqual(result['subscriber_number'], "123456")
 
     def test_nonexisting_number(self):
         msisdn = "99999999999"
-        result = ParseMSISDN.transform(msisdn)
         self.assertRaises(Exception, ParseMSISDN.transform, msisdn)
 
+    def test_nonexisting_number_with_leading_plus(self):
+        msisdn = "+99999999999"
+        self.assertRaises(Exception, ParseMSISDN.transform, msisdn)
+
+    def test_slovenian_number(self):
+        msisdn = "+38640658494"
+        result = ParseMSISDN.transform(msisdn)
+        self.assertEqual(result['country_identifier'], "SI")
+        self.assertEqual(result['country_dialing_code'], "386")
+        self.assertEqual(result['mno_identifier'], "Si.mobil")
+        self.assertEqual(result['subscriber_number'], "658494")
 
 if __name__ == '__main__':
     unittest.main()
